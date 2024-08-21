57935408beb4d83ad4345e47d75c0c426d03276d
Black Hawk <140146639+BlackHawk616@users.noreply.github.com>
2024-08-21 22:26:20 +0530

    Updated y.tsx
    fixed SVG

--- src/components/y.tsx
+++ src/components/y.tsx
@@ -393,19 +393,14 @@ export function y() {
   )
 }
 
-function SchoolIcon(props) {
+function SchoolIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
+      aria-hidden="true"
     >
       <path d="M14 22v-4a2 2 0 1 0-4 0v4" />
       <path d="m18 10 4 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-8l4-2" />
